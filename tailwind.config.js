71f13e1d4f1d7b98c726d63db9ed6d6411ec2a51
mohamed frouh <mohamedfrouh96@gmail.com>
2021-03-06 20:00:51 +0200

    install tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@ module.exports = {
   purge: [
     './resources/**/*.blade.php',
     './resources/**/*.js',
-    './resources/**/*.css',
     './resources/**/*.vue',
   ],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
